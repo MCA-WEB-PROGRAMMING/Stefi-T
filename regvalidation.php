786d8eb00c99385028e21a442f9e3c52518af2de
Stefi T <93776042+Stefi-T@users.noreply.github.com>
2022-01-29 18:03:12 +0530

    Add files via upload

--- regvalidation.php
+++ regvalidation.php
@@ -1,56 +1,3 @@
-
-<?php
-if(isset($_POST['submit']))
-{
-    $username=$_POST['username'];
-    $password=$_POST['password'];
-    $age=$_POST['age'];
-    $phone=$_POST['ph'];
-    $email=$_POST['em'];
-    $n1=preg_match('@[A-Z]@',$username);
-    $uppercase=preg_match('@[A-Z]@',$password);
-    $lowercase=preg_match('@[a-z]@',$password);
-    $number=preg_match('@[0-9]@',$password);
-    $p= preg_match('@[6-9]\d{9}@', $phone);
-    if($username=="")
-    {
-        echo("Please Enter the username!")."<br>";
-    }
-    elseif(!$n1)
-    {
-    echo("Only letters are allowed for the  Name Filed")."<br>";
-    }
-    if($password=="" && strlen($password)<8)
-    {
-        echo("please enter a password")."<br>";
-    }
-    elseif(!$uppercase || !$lowercase || !$number)
-    {
-        echo("please enter a valid password!")."<br>";
-    }
-    if($age=="")
-    {
-        echo("Please Enter your age!")."<br>";
-    }
-    if($phone=="")
-    {
-        echo("Please Enter a valid phone number!")."<br>";
-    }
-    elseif(!$p)
-    {
-        echo("phone number must contain 10 digits & only start with 6/7/8/9")."<br>";
-    }
-    if($email=="")
-    {
-        echo("Please Enter your mail id!")."<br>";
-    }
-    
-if (!filter_var($email, FILTER_VALIDATE_EMAIL))
- {
- echo("Invalid email format");
-}
-}
-?>
 <html>
 <head>
 <style>
@@ -98,6 +45,110 @@ if (!filter_var($email, FILTER_VALIDATE_EMAIL))
 </table>
 </div>
 </form>
+<table border="1">
+<tr>
+<th>Username</th>
+<th>Address</th>
+<th>Age</th>
+<th>Phone</th>
+<th>Email</th>
+</tr>
+
+
+<?php
+if(isset($_POST['submit']))
+{
+    $username=$_POST['username'];
+    $address=$_POST['add'];
+    $password=$_POST['password'];
+    $age=$_POST['age'];
+    $phone=$_POST['ph'];
+    $email=$_POST['em'];
+    $n1=preg_match('@[A-Z]@',$username);
+    $uppercase=preg_match('@[A-Z]@',$password);
+    $lowercase=preg_match('@[a-z]@',$password);
+    $number=preg_match('@[0-9]@',$password);
+    $p= preg_match('@[6-9]\d{9}@', $phone);
+    $conn=mysqli_connect("localhost","root","","Student");
+    if($conn)
+    {
+        echo("Successfully connected");
+        echo "<br>";
+    }
+    else{
+        echo("error");
+    }
+    if($username=="")
+    {
+        echo("Please Enter the username!")."<br>";
+    }
+    elseif(!$n1)
+    {
+    echo("Only letters are allowed for the  Name Filed")."<br>";
+    }
+    if($password=="" && strlen($password)<8)
+    {
+        echo("please enter a password")."<br>";
+    }
+    elseif(!$uppercase || !$lowercase || !$number)
+    {
+        echo("please enter a valid password!")."<br>";
+    }
+    if($age=="")
+    {
+        echo("Please Enter your age!")."<br>";
+    }
+    if($phone=="")
+    {
+        echo("Please Enter a valid phone number!")."<br>";
+    }
+    elseif(!$p)
+    {
+        echo("phone number must contain 10 digits & only start with 6/7/8/9")."<br>";
+    }
+    if($email=="")
+    {
+        echo("Please Enter your mail id!")."<br>";
+    }
+    
+if (!filter_var($email, FILTER_VALIDATE_EMAIL))
+ {
+ echo("Invalid email format");
+}
+$query="INSERT INTO reg(Username,Address,Age,Phone,Email,Password)VALUES('{$username}','{$address}','{$age}','{$phone}','{$email}','{$password}')";
+ 
+if(mysqli_query($conn,$query))
+{
+    echo("successfully inserted");
+    echo "<br>";
+}
+else
+{
+    echo("insertion failed");
+}
+ 
+                
+
+$s="SELECT * FROM reg";
+$data=mysqli_query($conn,$s);
+while($res=mysqli_fetch_assoc($data))
+{
+    ?>
+   <tr>
+    <td><?php echo $res['Username'];?></td>
+    <td><?php echo $res['Address'];?></td>
+    <td><?php echo $res['Age'];?></td>
+    <td><?php echo $res['Phone'];?></td>
+    <td><?php echo $res['Email'];?></td>
+</tr>
+<?php
+}
+
+}
+?>
+</table>
 </body>
 </html>
+
+
 
